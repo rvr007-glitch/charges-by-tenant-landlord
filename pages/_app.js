fce3b5bf3aece2228098a5a7550272570842bf79
SHIVAM3223 <shivambhushan653@gmail.com>
2022-01-06 17:21:16 +0530

    completed shifting

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,22 @@ import '../styles/p_auth.css'
 import '../styles/generateCharges.css'
 import "../styles/Profile.css";
 import "../styles/Landing.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+import { useEffect } from "react";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+  return <Component {...pageProps} />;
 }
+
+MyApp.getInitialProps = async (ctx) => {
+  const areLogsEnabled = ctx?.router?.query?.debug || "";
+  global.areLogsEnabled = areLogsEnabled === "true";
+  return {};
+};
 
 export default MyApp
