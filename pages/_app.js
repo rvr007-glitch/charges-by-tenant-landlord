5b06cd900be1b8b40cd1cfc38a57f93bdab4a470
SHIVAM3223 <shivambhushan653@gmail.com>
2022-01-06 17:06:27 +0530

    updated css and images folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import '../styles/p_auth.css'
 import '../styles/generateCharges.css'
+import "../styles/Profile.css";
+import "../styles/Landing.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
