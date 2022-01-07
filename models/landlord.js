98c824e6bf7d3eee97683bf78fb9d891cea20adc
rvr007-glitch <79240272+rvr007-glitch@users.noreply.github.com>
2022-01-07 19:08:07 +0530

    Landlord routes

--- models/landlord.js
+++ models/landlord.js
@@ -11,7 +11,7 @@ const landlordSchema = new mongoose.Schema({
     },
     username:{
         type:String,
-        required:[true,'Please enter a username'],
+       
         min:3,
         max:25,
         unique:true
@@ -24,7 +24,7 @@ const landlordSchema = new mongoose.Schema({
     },
     contact:{
         type:Number,
-        required:[true,'Please enter your contact number']
+        
     },
     password:{
         type:String,
@@ -33,31 +33,31 @@ const landlordSchema = new mongoose.Schema({
     },
     DOB:{
         type:Date,
-        required:true,
+        
         validate:[isDate,'Please enter a valid date']
     },
     address:[{
         first_line:{
             type:String,
-            required:[true,'Please enter your address'],
+           
             
         },
         city:{
             type:String,
-            required:[true,'Please enter your city']
+            
         },
         state:{
             type:String,
-            required:[true,'Please enter your state']
+           
         },
         Country:{
             type:String,
-            required:[true,'Please enter your country']
+       
         },
         pincode:{
-            type:Number,
-            required:[true,'Please enter your pincode'],
-            validate:[isPostalCode,'Please enter proper pin code']
+            type:String,
+         
+            
         },
         landmark:{
             type:String,
@@ -67,50 +67,50 @@ const landlordSchema = new mongoose.Schema({
     }],
     occupation:{
         type:String,
-        required:[true,'Please enter your occupation']
+        
     },
     verification:{
         type:String,
         enum:['Aadhar','VoterID','PanCard'],
-        required:[true,'Please enter your verification id']
+       
     },
-    account:[{
+    account:{
         acc_num:{
             type:String,
-            required:true
+            
         },
         ifsc:{
             type:String,
-            required:true
+            
         }
-    }],
+    },
     site_list:{
         type:Array
     }
 },{timestamps:true});
 
 //when landlord updates password
-landlordSchema.pre('save',(next)=>{
+// landlordSchema.pre('save',(next)=>{
 
-    if(!this.isModified('password')){
-        return next();
-    }
-    const user = this;
-    bcrypt.genSalt(10, function(err, salt){
-        if (err){ return next(err) }
+//     if(!this.isModified('password')){
+//         return next();
+//     }
+//     const user = this;
+//     bcrypt.genSalt(10, function(err, salt){
+//         if (err){ return next(err) }
 
-        bcrypt.hash(user.password, salt, null, function(err, hash){
-            if(err){return next(err)}
+//         bcrypt.hash(user.password, salt, null, function(err, hash){
+//             if(err){return next(err)}
 
-            else if(user.password == hash){
-                return next("new password cannot be same as previous password")
-            }
+//             else if(user.password == hash){
+//                 return next("new password cannot be same as previous password")
+//             }
 
-            user.password = hash;
-            next();
-        })
-    })
+//             user.password = hash;
+//             next();
+//         })
+//     })
 
-});
+// });
 
-module.exports = mongoose.model("Landlord",landlordSchema);
+module.exports =mongoose.models.Landlord|| mongoose.model("Landlord",landlordSchema);
---
+++ pages/api/auth/users/login.js
@@ -0,0 +1,51 @@
+import { Logger } from "log4js";
+import { connectToDatabase } from "../../../../db/connect"
+import { sendError } from "../../../../helpers/help";
+import { sendSuccess } from "../../../../helpers/help";
+var constants = require("../../../../helpers/constants")
+var Landlord = require("../../../../models/landlord")
+const {isEmail} = require("validator");
+const jwt = require("jsonwebtoken")
+
+const bcrypt = require("bcrypt");
+const config = require("../../../../config/config");
+
+
+
+export default async function handler(req,res){
+    if(req.method === "POST"){
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+      // Split at the space
+      const bearer = bearerHeader.split(' ');
+      // Get token from array
+      const bearerToken = bearer[1];
+      // Set the token
+      req.token = bearerToken;
+      // Next middleware
+      jwt.verify(req.token, config.SECRET_KEY, (err,authData) => {
+        if(err)return sendError(res,err,constants.JWT_VERIFY)
+        Landlord.findById(authData.id, function(err,data){
+          if(err){
+              return sendError(res,err,constants.SERVER_ERROR)
+          }
+          else if(!data){
+              return sendError(res,"No Account Exist",constants.ACCOUNT_NOT_EXIST)
+          }
+          authData.profile=data;
+          return sendSuccess(res, authData)
+         
+      })
+    })
+      
+    } else {
+     
+      Logger.error()
+      return sendError(res,"Token not provided",constants.NULL_TOKEN)
+    }
+}
+else{
+  return sendError(res, "server error", 500)
+}
+}
