8600861d4513ee7e2ea22fecf5910d74b04963f1
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2022-01-18 11:29:11 +0530

    update

--- models/history.js
+++ models/history.js
@@ -5,45 +5,28 @@ const {isEmail,isDate} = require("validator");
 const historySchema = new mongoose.Schema({
     tenant_id:{
         type:mongoose.Schema.Types.ObjectId,
-        requred:true
+        ref:"Tenant"
     },
     site_id:{
         type:mongoose.Schema.Types.ObjectId,
-        requred:true
+        ref:"Sites",
+        required:true
     },
+    status:{
+        type:String,
+        default:"0"
+    },
+    
     requested_at:{
         type:Date,
     },
     joined_at:{
         type:Date,
     },
-    leave_at:{
+    left_at:{
         type:Date,
     }
 
 },{timestamps:true})
-
-historySchema.pre("save",(next) => {
-
-if(this.requested('requested_at')){
-   this.requested_at = Date.now;
-   next(); 
-}
-
-else if(this.joined('joined_at')){
-    this.joined_at = Date.now;
-    next(); 
-}
-
-else if(this.left('leave_at')){
-    this.leave_at = Date.now;
-    next(); 
-}
-
-else{
-    return next();
-}
-
-})
 
 module.exports = mongoose.model("History",historySchema);
