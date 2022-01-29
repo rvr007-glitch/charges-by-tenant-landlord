9eb916a933abfd013405d8690636c28c5e09bad4
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2022-01-29 11:31:35 +0530

    Route to get all the charges that has been paid to the landlord

---
+++ pages/api/charges/paid.js
@@ -0,0 +1,31 @@
+import { sendSuccess, sendError } from "../../../helpers/help";
+var constants = require("../../../helpers/constants");
+import connectMongoDb from "../../../db/connect";
+
+var Landlord = require("../../../models/landlord");
+var Transaction = require("../../../models/transaction")
+var Charge = require("../../../models/Charge")
+const { isEmail } = require("validator");
+const jwt = require("jsonwebtoken");
+
+const bcrypt = require("bcrypt");
+const config = require("../../../config/config");
+import { auth } from "../../../utility/auth";
+
+export default async function handler(req, res){
+    if(req.method === "POST"){
+        var landlordId;
+        auth(req, res, (err, authData) => {
+            if(err) return sendError(res, err.message, 500)
+            landlordId = authData.id
+        })
+
+        var data = await Transaction.find({landlord_id: landlordId}).populate('charge_id', {'isPaid': true})
+        if(data){
+            return sendSuccess(res, data)
+        }
+        else{
+            return sendError(res, "error in finding paid data", 500)
+        }
+    }
+}
